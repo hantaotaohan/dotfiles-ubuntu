77dbeeeae4eb229759dc90e87ce744f4b44bacff
TaoTao <hantaotaohan@gmail.com>
2019-11-26 15:27:21 +0800

    Update run.sh

--- install/run.sh
+++ install/run.sh
@@ -1,2 +1,4 @@
 git clone https://github.com/hantaotaohan/Dotfiles.git
-sudo bash ./Dotfiles/install/install_server.sh
+sudo ln -fs /Dotfiles/install/install_server.sh $HOME/
+chmod 777 $HOME/install_server.sh
+./install_server.sh
