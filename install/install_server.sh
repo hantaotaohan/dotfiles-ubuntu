754df016e4036a8b4b3f40a88fd3abd05634a46e
TaoTao <hantaotaohan@gmail.com>
2019-12-04 15:09:50 +0800

    Update install_server.sh

--- install/install_server.sh
+++ install/install_server.sh
@@ -44,99 +44,99 @@ Dotfiles_copy=(.config .vim z.sh)
 #-------------------------------------------------------------------
 
 sudo ln -sf "$Dotfiles_repo/sources.list" "/etc/apt/sources.list"
-echo -e "${blue}Repace sources.list Done !"
+echo -e "\t[+]${blue}Repace sources.list Done !"
 
 #-------------------------------------------------------------------
 # Update Upgrade Packages
 #-------------------------------------------------------------------
 
 sudo apt update -y -qq > /dev/null 2>&1
-echo -e "${blue}Update Successful"
+echo -e "\t[+]${blue}Update Successful"
 sudo apt upgrade -y -qq > /dev/null 2>&1
-echo -e "${blue}Upgrade Successful"
+echo -e "\t[+]${blue}Upgrade Successful"
 
 #-------------------------------------------------------------------
 # Install Base System
 #-------------------------------------------------------------------
 
 sudo apt install -y -qq wget > /dev/null 2>&1
-echo -e "${blue}Wget Successful"
+echo -e "\t[+]${blue}Wget Successful"
 
 sudo apt install -y -qq git > /dev/null 2>&1
-echo -e "${blue}Git Successful"
+echo -e "\t[+]${blue}Git Successful"
 
 sudo apt install -y -qq python3-pip > /dev/null 2>&1
-echo -e "${blue}Python3-pip Successful"
+echo -e "\t[+]${blue}Python3-pip Successful"
 
 sudo apt install -y -qq python-pip > /dev/null 2>&1
-echo -e "${blue}Python2-pip Successful"
+echo -e "\t[+]${blue}Python2-pip Successful"
 
 sudo apt install -y -qq git > /dev/null 2>&1
-echo -e "${blue}Git Successful"
+echo -e "\t[+]${blue}Git Successful"
 
 sudo apt install -y -qq curl > /dev/null 2>&1
-echo -e "${blue}Curl Successful"
+echo -e "\t[+]${blue}Curl Successful"
 
 sudo apt install -y -qq ctags > /dev/null 2>&1
-echo -e "${blue}Ctags Successful"
+echo -e "\t[+]${blue}Ctags Successful"
 
 sudo apt install -y -qq vim > /dev/null 2>&1
-echo -e "${blue}Vim Successful"
+echo -e "\t[+]${blue}Vim Successful"
 
 sudo apt install -y -qq tmux > /dev/null 2>&1
-echo -e "${blue}Tmux Successful"
+echo -e "\t[+]${blue}Tmux Successful"
 
 sudo apt install -y -qq silversearcher-ag > /dev/null 2>&1
-echo -e "${blue}Ag Successful"
+echo -e "\t[+]${blue}Ag Successful"
 
 sudo apt install -y -qq w3m > /dev/null 2>&1
-echo -e "${blue}W3m Successful"
+echo -e "\t[+]${blue}W3m Successful"
 
 sudo apt install -y -qq unzip > /dev/null 2>&1
-echo -e "${blue}Unzip Successful"
+echo -e "\t[+]${blue}Unzip Successful"
 
 #-------------------------------------------------------------------
 # Install Openssh-Server
 #-------------------------------------------------------------------
 
 sudo apt install -y -qq openssh-server > /dev/null 2>&1
-echo -e "${blue}Openssh-server Successful"
+echo -e "\t[+]${blue}Openssh-server Successful"
 
 sudo apt install -y -qq openssh-client > /dev/null 2>&1
-echo -e "${blue}Openssh-client Successful"
+echo -e "\t[+]${blue}Openssh-client Successful"
 
 #-------------------------------------------------------------------
 # Install X Apps 
 #-------------------------------------------------------------------
 
 sudo apt install -y -qq xvfb > /dev/null 2>&1
-echo -e "${blue}Xvfb Successful"
+echo -e "\t[+]${blue}Xvfb Successful"
 
 #-------------------------------------------------------------------
 # Pip3 install Apps
 #-------------------------------------------------------------------
 
 sudo pip3 install -q powerline-shell
-echo -e "${blue}Powerline-shell Successful"
+echo -e "\t[+]${blue}Powerline-shell Successful"
 
 sudo pip3 install -q pandas
-echo -e "${blue}Pandas Successful"
+echo -e "\t[+]${blue}Pandas Successful"
 
 sudo pip3 install -q django
-echo -e "${blue}Django Successful"
+echo -e "\t[+]${blue}Django Successful"
 
 sudo pip3 install -q pyvirtualdisplay 
-echo -e "${blue}Pyvirtualdisplay Successful"
+echo -e "\t[+]${blue}Pyvirtualdisplay Successful"
 
 sudo pip3 install -q selenium
-echo -e "${blue}Selenium Successful"
+echo -e "\t[+]${blue}Selenium Successful"
 
 #-------------------------------------------------------------------
 # Update
 #-------------------------------------------------------------------
 
 sudo apt update -y -qq > /dev/null 2>&1
-echo -e "${blue}Again Update Successful"
+echo -e "\t[+]${blue}Again Update Successful"
 
 #-------------------------------------------------------------------
 # Install Dotfiles.
@@ -147,7 +147,7 @@ for dots_home in "${Dotfiles_home[@]}"
         sudo rm -rf "$HOME/${dots_home}"
         sudo ln -fs "$Dotfiles_repo/${dots_home}" "$HOME/"
     done
-echo -e "${blue}Install Dotfiles Successful"
+echo -e "\t[+]${blue}Install Dotfiles Successful"
 
 #-------------------------------------------------------------------
 # Install Dotfiles.COPY
@@ -158,7 +158,7 @@ for dots_copy in "${Dotfiles_copy[@]}"
         sudo rm -rf "$HOME/${dots_copy}"
         sudo cp -rf "$Dotfiles_repo/${dots_copy}" "$HOME/"
     done
-echo -e "${blue}Install Dotfiles COPY Successful"
+echo -e "\t[+]${blue}Install Dotfiles COPY Successful"
 
 #-------------------------------------------------------------------
 # Chmod NeedFlods
@@ -168,14 +168,14 @@ if [ -f "$HOME/.vim" ];then
     sudo chmod +x $HOME/.vim
 fi
 
-echo -e "${blue}Chomd Needfolds Successful"
+echo -e "\t[+]${blue}Chomd Needfolds Successful"
 
 #-------------------------------------------------------------------
 # Install Vim Plug
 #-------------------------------------------------------------------
 
 sudo vim
-echo -e "${blue}Vim PlugInstall Successful"
+echo -e "\t[+]${blue}Vim PlugInstall Successful"
 
 #-------------------------------------------------------------------
 # Chmod NeedFlods
@@ -190,33 +190,33 @@ fi
 #-------------------------------------------------------------------
 
 sudo dpkg -i $Dotfiles_repo/ripgrep/ripgrep_11.0.2_amd64.deb > /dev/null 2>&1
-echo -e "${blue}Ripgrep Install success"
+echo -e "\t[+]${blue}Ripgrep Install success"
 
 #-------------------------------------------------------------------
 # Install Bat
 #-------------------------------------------------------------------
 
 sudo dpkg -i $Dotfiles_repo/bat/bat_0.12.1_amd64.deb > /dev/null 2>&1
-echo -e "${blue}Bat Install success"
+echo -e "\t[+]${blue}Bat Install success"
 
 #-------------------------------------------------------------------
 # Install Chrome and ChromeDriver
 #-------------------------------------------------------------------
 
 sudo wget -q -t 0 -P $HOME https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
-echo -e "${blue}Downloding Chrome"
+echo -e "\t[+]${blue}Downloding Chrome"
 
 sudo dpkg -i $HOME/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
-echo -e "${blue}Install Chrome ............"
+echo -e "\t[+]${blue}Install Chrome ............"
 
 sudo rm -rf $HOME/google*
-echo -e "${blue}Delete Chrome ............"
+echo -e "\t[+]${blue}Delete Chrome ............"
 
 sudo apt --fix-broken install -y -qq > /dev/null 2>&1
-echo -e "${blue}Install Chrome Requirements"
+echo -e "\t[+]${blue}Install Chrome Requirements"
 
 sudo dpkg -i $Dotfiles_repo/chrome/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
-echo -e "${blue}Chrome Install Successful"
+echo -e "\t[+]${blue}Chrome Install Successful"
 
 if [ -f "/usr/local/share/chromedriver" ];then
 sudo rm -rf /usr/local/share/chromedriver
@@ -231,7 +231,7 @@ sudo ln -sf /usr/local/share/chromedriver /usr/local/bin/chromedriver
 sudo ln -sf /usr/local/share/chromedriver /usr/bin/chromedriver
 fi
 
-echo -e "${blue}ChromeDriver Install Successful"
+echo -e "\t[+]${blue}ChromeDriver Install Successful"
 
 #-------------------------------------------------------------------
 # .tmux
@@ -249,7 +249,7 @@ tmux new-session -d
 $HOME/.tmux/plugins/tpm/scripts/install_plugins.sh > /dev/null 2>&1
 tmux kill-server
 
-echo -e "${blue}TPM Successful"
+echo -e "\t[+]${blue}TPM Successful"
 
 
 #-------------------------------------------------------------------
@@ -265,21 +265,21 @@ else
     sudo chmod 777 /usr/local/bin/diff-so-fancy
 fi
 
-echo -e "${blue}Diff-So-Fancy Successful"
+echo -e "\t[+]${blue}Diff-So-Fancy Successful"
 
 #-------------------------------------------------------------------
 # AutoRemove
 #-------------------------------------------------------------------
 
 sudo apt autoremove -y -qq > /dev/null 2>&1
-echo -e "${blue}AutoRemove Successful"
+echo -e "\t[+]${blue}AutoRemove Successful"
 
 #-------------------------------------------------------------------
 # Set TimeZone
 #-------------------------------------------------------------------
 
 # sudo timedatectl set-timezone "Asia/Shanghai"
-# echo -e "${blue}Set TimeZone is Successful"
+# echo -e "\t[+]${blue}Set TimeZone is Successful"
 
 #-------------------------------------------------------------------
 # End
