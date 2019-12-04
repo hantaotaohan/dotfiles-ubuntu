7e038692cae6bbbede646d4d90e7c68e74caac8b
TaoTao <hantaotaohan@gmail.com>
2019-12-04 13:59:21 +0800

    Update install_server.sh

--- install/install_server.sh
+++ install/install_server.sh
@@ -238,11 +238,11 @@ echo -e "${blue}TPM Successful"
 #-------------------------------------------------------------------
 
 if [ ! -f "/usr/local/bin/diff-so-fancy" ];then
-    sudo cp -f $Dotfiles_repo/diff-so-fancy /usr/loacl/bin
+    sudo cp -f $Dotfiles_repo/diff-so-fancy /usr/local/bin
     sudo chmod 777 diff-so-fancy
 else
     sudo rm -rf /usr/loacl/bin/diff-so-fancy
-    sudo cp -f $Dotfiles_repo/diff-so-fancy /usr/loacl/bin
+    sudo cp -f $Dotfiles_repo/diff-so-fancy /usr/local/bin
     sudo chmod 777 diff-so-fancy
 fi
 
