c419f156f333bbf3f108e143c172f94861bda536
TaoTao <hantaotaohan@gmail.com>
2019-12-04 15:35:03 +0800

    Update run.sh

--- install/run.sh
+++ install/run.sh
@@ -1,3 +1,9 @@
+sudo rm -rf /etc/apt/sources.list
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse" > /etc/apt/sources.list
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse" >> /etc/apt/sources.list
+sudo update
 sudo apt install git
 git clone https://github.com/hantaotaohan/Dotfiles.git
 cd $HOME/Dotfiles/install
