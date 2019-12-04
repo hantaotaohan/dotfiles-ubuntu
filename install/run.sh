0c2dc425dcc2a95db9b3739c9f5e2bc84180c4c8
TaoTao <hantaotaohan@gmail.com>
2019-12-04 15:43:37 +0800

    Update run.sh

--- install/run.sh
+++ install/run.sh
@@ -1,10 +1,10 @@
-sudo rm -rf /etc/apt/sources.list
-echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse" > /etc/apt/sources.list
-echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse" >> /etc/apt/sources.list
-echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse" >> /etc/apt/sources.list
-echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse" >> /etc/apt/sources.list
-sudo apt update -y
-sudo apt install git
-git clone https://github.com/hantaotaohan/Dotfiles.git
-cd $HOME/Dotfiles/install
+sudo rm -rf /etc/apt/sources.list &&\
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse" > /etc/apt/sources.list &&\
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse" >> /etc/apt/sources.list &&\
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse" >> /etc/apt/sources.list &&\
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse" >> /etc/apt/sources.list &&\
+sudo apt-get update -y &&\
+sudo apt install git &&\
+git clone https://github.com/hantaotaohan/Dotfiles.git &&\
+cd $HOME/Dotfiles/install &&\
 sudo ./install_server.sh
