92b16333ef5e5fbc8918b402f85532e3085cb6e7
TaoTao <hantaotaohan@gmail.com>
2019-12-05 15:07:39 +0800

    Update install_desktop.sh

--- install/install_desktop.sh
+++ install/install_desktop.sh
@@ -1,158 +1,309 @@
 #!/bin/bash
 
+clear
+
+
+#-------------------------------------------------------------------
+# Start
 #-------------------------------------------------------------------
 
+echo ""
+echo "                                                                   ";
+echo "               ___  _   _  ____   _____     _     _      _         ";
+echo "              |_ _|| \ | |/ ___| |_   _|   / \   | |    | |        ";
+echo "               | | |  \| |\___ \   | |    / _ \  | |    | |        ";
+echo "               | | | |\  | ___) |  | |   / ___ \ | |___ | |___     ";
+echo "              |___||_| \_||____/   |_|  /_/   \_\|_____||_____|    ";
+echo "                                                                   ";                                                                                    
+echo ""
+echo "--------------------------------------------------------------------"
+echo -e " \t\tTaoTao - Saber - Ubuntu - Desktop - Install                 "
+echo "--------------------------------------------------------------------"
+echo ""
+
+#-------------------------------------------------------------------
+# Setting Colors
+#-------------------------------------------------------------------
+
+blue='\e[0;34m'
+green='\e[0;32m'
+
+#-------------------------------------------------------------------
 # Setting Variable
-Dotfiles_repo=$(pwd)
-Dotfiles_home=(.vim .tmux .w3m .bashrc .profile .tmux.conf .vimrc .xmodmap z.sh)
-Dotfiles_xdg=(.config)
-
 #-------------------------------------------------------------------
 
+Dotfiles_repo=$(dirname $PWD)
+Dotfiles_home=(.w3m .bashrc .bash_profile 
+    .bash_prompt .tmux.conf .vimrc .xmodmap .bash_aliases 
+    .curlrc .inputrc .gitconfig .exports .ripgreprc .wgetrc 
+    .dircolors .Xresources)
+Dotfiles_copy=(.config .vim z.sh)
+
+#-------------------------------------------------------------------
 # Repace Sources
+#-------------------------------------------------------------------
+
 sudo ln -sf "$Dotfiles_repo/sources.list" "/etc/apt/sources.list"
-echo "Repace sources.list Done !"
+echo -e "\t[+]${blue}Repace sources.list Done !"
 
 #-------------------------------------------------------------------
-
 # Update Upgrade Packages
-sudo apt update -y
-sudo apt upgrade -y
-
 #-------------------------------------------------------------------
 
+sudo apt update -y -qq > /dev/null 2>&1
+echo -e "\t[+]${blue}Update Successful"
+sudo apt upgrade -y -qq > /dev/null 2>&1
+echo -e "\t[+]${blue}Upgrade Successful"
+
+#-------------------------------------------------------------------
 # Install Base System
-sudo apt install -y wget
-sudo apt install -y python3-pip 
-sudo apt install -y python-pip 
-sudo apt install -y git 
-sudo apt install -y curl 
-sudo apt install -y ctags 
-sudo apt install -y vim 
-sudo apt install -y tmux 
-sudo apt install -y silversearcher-ag
-sudo apt install -y w3m
-sudo apt install -y unzip
-
 #-------------------------------------------------------------------
 
+sudo apt install -y -qq wget > /dev/null 2>&1
+echo -e "\t[+]${blue}Wget Successful"
+
+sudo apt install -y -qq xterm > /dev/null 2>&1
+echo -e "\t[+]${blue}Git Successful"
+
+sudo apt install -y -qq git > /dev/null 2>&1
+echo -e "\t[+]${blue}Git Successful"
+
+sudo apt install -y -qq python3-pip > /dev/null 2>&1
+echo -e "\t[+]${blue}Python3-pip Successful"
+
+sudo apt install -y -qq python-pip > /dev/null 2>&1
+echo -e "\t[+]${blue}Python2-pip Successful"
+
+sudo apt install -y -qq git > /dev/null 2>&1
+echo -e "\t[+]${blue}Git Successful"
+
+sudo apt install -y -qq curl > /dev/null 2>&1
+echo -e "\t[+]${blue}Curl Successful"
+
+sudo apt install -y -qq ctags > /dev/null 2>&1
+echo -e "\t[+]${blue}Ctags Successful"
+
+sudo apt install -y -qq vim > /dev/null 2>&1
+echo -e "\t[+]${blue}Vim Successful"
+
+sudo apt install -y -qq tmux > /dev/null 2>&1
+echo -e "\t[+]${blue}Tmux Successful"
+
+sudo apt install -y -qq silversearcher-ag > /dev/null 2>&1
+echo -e "\t[+]${blue}Ag Successful"
+
+sudo apt install -y -qq w3m > /dev/null 2>&1
+echo -e "\t[+]${blue}W3m Successful"
+
+sudo apt install -y -qq unzip > /dev/null 2>&1
+echo -e "\t[+]${blue}Unzip Successful"
+
+#-------------------------------------------------------------------
 # Install Openssh-Server
-sudo apt install -y openssh-server
-sudo apt install -y openssh-client
-
 #-------------------------------------------------------------------
 
+sudo apt install -y -qq openssh-server > /dev/null 2>&1
+echo -e "\t[+]${blue}Openssh-server Successful"
+
+sudo apt install -y -qq openssh-client > /dev/null 2>&1
+echo -e "\t[+]${blue}Openssh-client Successful"
+
+#-------------------------------------------------------------------
 # Install X Apps 
-sudo apt install -y xvfb
-sudo apt install -y xserver-xorg
-sudo apt install -y x11-apps
-sudo apt install -y x11-utils
-sudo apt install -y x11-xserver-utils
-sudo apt install -y x11vnc
-
 #-------------------------------------------------------------------
 
+sudo apt install -y -qq xvfb > /dev/null 2>&1
+echo -e "\t[+]${blue}Xvfb Successful"
+
+#-------------------------------------------------------------------
 # Pip3 install Apps
-sudo pip3 install powerline-shell
-sudo pip3 install pandas
-sudo pip3 install django
-sudo pip3 install pyvirtualdisplay 
-sudo pip3 install selenium
-
 #-------------------------------------------------------------------
 
-# Install config.
+sudo pip3 install -q powerline-shell
+echo -e "\t[+]${blue}Powerline-shell Successful"
+
+sudo pip3 install -q pandas
+echo -e "\t[+]${blue}Pandas Successful"
+
+sudo pip3 install -q django
+echo -e "\t[+]${blue}Django Successful"
+
+sudo pip3 install -q pyvirtualdisplay 
+echo -e "\t[+]${blue}Pyvirtualdisplay Successful"
+
+sudo pip3 install -q selenium
+echo -e "\t[+]${blue}Selenium Successful"
+
+#-------------------------------------------------------------------
+# Update
+#-------------------------------------------------------------------
+
+sudo apt update -y -qq > /dev/null 2>&1
+echo -e "\t[+]${blue}Again Update Successful"
+
+#-------------------------------------------------------------------
+# Install Dotfiles.
+#-------------------------------------------------------------------
 
 for dots_home in "${Dotfiles_home[@]}"
     do
-        env rm -rf "$HOME/${dots_home}"
-        env ln -fs "$Dotfiles_repo/${dots_home}" "$HOME/"
+        sudo rm -rf "$HOME/${dots_home}"
+        sudo ln -fs "$Dotfiles_repo/${dots_home}" "$HOME/"
     done
+echo -e "\t[+]${blue}Install Dotfiles Successful"
 
 #-------------------------------------------------------------------
+# Install Dotfiles.COPY
+#-------------------------------------------------------------------
 
-# Install config to ~/.config.
-
-if [ -f "/etc/X11/xinit" ];then
-    mkdir -p "$HOME/.config"
-    for dots_xdg_conf in "${Dotfiles_xdg/[@]}"
+for dots_copy in "${Dotfiles_copy[@]}"
     do
-        env rm -rf "$HOME/.config/${dots_xdg_conf[*]//./}"
-        env ln -fs "$Dotfiles_repo/${dots_xdg_conf}" "$HOME/.config/${dots_xdg_conf[*]//./}"
+        sudo rm -rf "$HOME/${dots_copy}"
+        sudo cp -rf "$Dotfiles_repo/${dots_copy}" "$HOME/"
     done
-fi
+echo -e "\t[+]${blue}Install Dotfiles COPY Successful"
 
 #-------------------------------------------------------------------
-
-# Install Fonts
-
-if test "$(uname)" = "Darwin" ; then
-  # Linux
-  if [ -f "/etc/X11/xinit" ];then
-  font_dir="$HOME/.local/share/fonts"
-  mkdir -p $font_dir
-  fi
-fi
-
-# Copy all fonts to user fonts directory
-
-echo "Copying fonts..."
-find "$Dotfiles_repo" \( -name "*.ttf" \) -type f -print0 | xargs -0 -n1 -I % cp "%" "$font_dir/"
-
-# Reset font cache on Linux
-
-if which fc-cache >/dev/null 2>&1 ; then
-    echo "Resetting font cache, this may take a moment..."
-    fc-cache -f "$font_dir"
-fi
-echo "Fonts installed to $font_dir"
-
+# Chmod NeedFlods
 #-------------------------------------------------------------------
 
+if [ -f "$HOME/.vim" ];then
+    sudo chmod +x $HOME/.vim
+fi
+
+echo -e "\t[+]${blue}Chomd Needfolds Successful"
+
+#-------------------------------------------------------------------
 # Install Vim Plug
+#-------------------------------------------------------------------
 
-vim -c "PlugInstall" 
+sudo vim
+echo -e "\t[+]${blue}Vim PlugInstall Successful"
 
+#-------------------------------------------------------------------
+# Chmod NeedFlods
+#-------------------------------------------------------------------
 
 if [ -f "$HOME/.viminfo" ];then
-sudo chmod 777 $HOME/.viminfo
+    sudo chmod 777 $HOME/.viminfo
 fi
 
+#-------------------------------------------------------------------
+# Install Ripgrep
+#-------------------------------------------------------------------
+
+sudo dpkg -i $Dotfiles_repo/ripgrep/ripgrep_11.0.2_amd64.deb > /dev/null 2>&1
+echo -e "\t[+]${blue}Ripgrep Install success"
+
+#-------------------------------------------------------------------
+# Install Bat
+#-------------------------------------------------------------------
+
+sudo dpkg -i $Dotfiles_repo/bat/bat_0.12.1_amd64.deb > /dev/null 2>&1
+echo -e "\t[+]${blue}Bat Install success"
+
+#-------------------------------------------------------------------
+# Install Chrome and ChromeDriver
+#-------------------------------------------------------------------
+
+sudo wget -q -t 0 -P $HOME https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
+echo -e "\t[+]${blue}Downloding Chrome"
+
+sudo dpkg -i $HOME/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
+echo -e "\t[+]${blue}Install Chrome ............"
+
+sudo rm -rf $HOME/google*
+echo -e "\t[+]${blue}Delete Chrome ............"
+
+sudo apt --fix-broken install -y -qq > /dev/null 2>&1
+echo -e "\t[+]${blue}Install Chrome Requirements"
+
+sudo dpkg -i $Dotfiles_repo/chrome/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
+echo -e "\t[+]${blue}Chrome Install Successful"
+
+if [ -f "/usr/local/share/chromedriver" ];then
+sudo rm -rf /usr/local/share/chromedriver
+sudo cp -f $Dotfiles_repo/chrome/chromedriver /usr/local/share/chromedriver
+sudo chmod +x /usr/local/share/chromedriver
+sudo ln -sf /usr/local/share/chromedriver /usr/local/bin/chromedriver
+sudo ln -sf /usr/local/share/chromedriver /usr/bin/chromedriver
+else
+sudo cp -f $Dotfiles_repo/chrome/chromedriver /usr/local/share/chromedriver
+sudo chmod +x /usr/local/share/chromedriver
+sudo ln -sf /usr/local/share/chromedriver /usr/local/bin/chromedriver
+sudo ln -sf /usr/local/share/chromedriver /usr/bin/chromedriver
+fi
+
+echo -e "\t[+]${blue}ChromeDriver Install Successful"
+
+#-------------------------------------------------------------------
+# .tmux
+#-------------------------------------------------------------------
+
+if [ ! -d "$HOME/.tmux" ];then
+    git clone -q https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+else
+    sudo rm -rf $HOME/.tmux
+    git clone -q https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+fi
+
+tmux start-server
+tmux new-session -d
+$HOME/.tmux/plugins/tpm/scripts/install_plugins.sh > /dev/null 2>&1
+tmux kill-server
+
+echo -e "\t[+]${blue}TPM Successful"
 
 
+#-------------------------------------------------------------------
+# diff-so-fancy
+#-------------------------------------------------------------------
 
+if [ ! -f "/usr/local/bin/diff-so-fancy" ];then
+    sudo cp -f $Dotfiles_repo/diff-so-fancy /usr/local/bin
+    sudo chmod 777 /usr/local/bin/diff-so-fancy
+else
+    sudo rm -rf /usr/local/bin/diff-so-fancy
+    sudo cp -f $Dotfiles_repo/diff-so-fancy /usr/local/bin
+    sudo chmod 777 /usr/local/bin/diff-so-fancy
+fi
 
-# Install Chrome ChromeDriver
-#sudo dpkg -i google-chrome-stable_current_amd64.deb
-#unzip chromedriver_linux64.zip
-#chmod +x chromedriver
-#sudo mv -f chromedriver /usr/local/share/chromedriver
-#sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
-#sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
+echo -e "\t[+]${blue}Diff-So-Fancy Successful"
 
+#-------------------------------------------------------------------
+# AutoRemove
+#-------------------------------------------------------------------
 
+sudo apt autoremove -y -qq > /dev/null 2>&1
+echo -e "\t[+]${blue}AutoRemove Successful"
 
+#-------------------------------------------------------------------
+# Set Xrdb
+#-------------------------------------------------------------------
 
+xrdb ~/.Xresources 
+echo -e "\t[+]${blue}Set Xrdb Successful"
 
+#-------------------------------------------------------------------
+# Set TimeZone
+#-------------------------------------------------------------------
 
+# sudo timedatectl set-timezone "Asia/Shanghai"
+# echo -e "\t[+]${blue}Set TimeZone is Successful"
 
+#-------------------------------------------------------------------
+# End
+#-------------------------------------------------------------------
 
-#================================================
-#if [ -f "/etc/apt/sources.list" ];then
-#echo "File exists - Delete Sources.list and Create /etc/apt/sources.list ."
-#rm -rf /etc/apt/sources.list
-#sudo mv sources.list /etc/apt/sources.list
-#echo "Repace Sources Done !"
-#else
-#echo "file does not exist - Create /etc/apt/sources.list ."
-#sudo mv sources.list /etc/apt/sources.list
-#echo "Repace Sources Done !"
-#fi
+echo ""
+echo -e "${green} - All Install Success - "
+echo ""
 
+#-------------------------------------------------------------------
+# Restart Bash
+#-------------------------------------------------------------------
 
-
-
-
+bash
 
 
 
