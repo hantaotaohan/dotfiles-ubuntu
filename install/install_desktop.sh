db379045ddf67fa40fba1fd18be7a9568c6f24d5
TaoTao <hantaotaohan@gmail.com>
2019-12-10 10:01:12 +0800

    Update install_desktop.sh

--- install/install_desktop.sh
+++ install/install_desktop.sh
@@ -354,6 +354,7 @@ echo -e "\t[+]${blue}Chown Successful"
 git clone https://github.com/meskarune/i3lock-fancy.git ~/i3lock-fancy
 cd ~/i3lock-fancy
 sudo make install
+sudo rm -rf ~/i3lock-fancy
 
 echo -e "\t[+]${blue}I3-lock-fancy Successful"
 
