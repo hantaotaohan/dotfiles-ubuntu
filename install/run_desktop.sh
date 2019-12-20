a9afb87759c5d6ab4c3dac4c16fa34dbb36cbd3f
TaoTao <hantaotaohan@gmail.com>
2019-12-20 09:57:18 +0800

    Update run_desktop.sh

--- install/run_desktop.sh
+++ install/run_desktop.sh
@@ -1,8 +1,8 @@
-#sudo rm -rf /etc/apt/sources.list &&\
-echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse" > /etc/apt/sources.list &&\
-echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse" >> /etc/apt/sources.list &&\
-echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse" >> /etc/apt/sources.list &&\
-echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse" >> /etc/apt/sources.list &&\
+sudo rm -rf /etc/apt/sources.list &&\
+sudo echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse" > /etc/apt/sources.list &&\
+sudo echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse" >> /etc/apt/sources.list &&\
+sudo echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse" >> /etc/apt/sources.list &&\
+sudo echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse" >> /etc/apt/sources.list &&\
 sudo apt-get update -y &&\
 sudo apt install git -y &&\
 git clone https://github.com/hantaotaohan/Dotfiles.git &&\
