0e6929be182824b8a6b5340df03c852fa2bf1c02
TaoTao <hantaotaohan@gmail.com>
2019-12-20 09:56:14 +0800

    Update run_desktop.sh

--- install/run_desktop.sh
+++ install/run_desktop.sh
@@ -1,4 +1,4 @@
-sudo rm -rf /etc/apt/sources.list &&\
+#sudo rm -rf /etc/apt/sources.list &&\
 echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse" > /etc/apt/sources.list &&\
 echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse" >> /etc/apt/sources.list &&\
 echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse" >> /etc/apt/sources.list &&\
