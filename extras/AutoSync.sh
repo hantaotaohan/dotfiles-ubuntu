1a0e4d8be42f94e8ddaa22ad834f22b3003727f4
TaoTao <hantaotaohan@gmail.com>
2020-07-27 16:21:36 +0800

    Update AutoSync.sh

--- extras/AutoSync.sh
+++ extras/AutoSync.sh
@@ -3,10 +3,19 @@
 Path1=$HOME/Vimwiki/src/img/
 Path2=$HOME/Vimwiki/docs/img/
 
+
 monitor() {
-  inotifywait -mrq --format '%w%f' -e create,close_write,delete $1 | while read
+  inotifywait -mrq --format '%w%f' -e create,close_write,delete $1 | while read line; do
         rsync -rtvu --delete ${Path1} ${Path2}
   done
 }
+
+
+processName="AutoSync.sh"
+processNum=`ps -aef |grep "${processName}" | wc -l`
+
+if [ "${processNum}" -gt "3" ]; then
+  exit
+fi
 
 monitor ${Path1};
