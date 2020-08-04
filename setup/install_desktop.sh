02b4de39025243d0e1f286b80b730e8efb5dd913
TaoTao <hantaotaohan@gmail.com>
2020-08-04 10:10:33 +0800

    Update install_desktop.sh

--- setup/install_desktop.sh
+++ setup/install_desktop.sh
@@ -409,7 +409,8 @@ echo -e "                                                                       
         pandas\
         django\
         pyvirtualdisplay\
-        selenium
+        selenium\
+	xlsx2csv
         )
 	for app in "${pipApps[@]}"
 	do
