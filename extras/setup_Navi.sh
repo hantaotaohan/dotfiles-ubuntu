49dfa471c709ed9d4de9640fdf57d031e3336c02
hantaotaohan <hantaotaohan@gmail.com>
2020-08-10 17:14:49 +0800

    sync

--- extras/setup_Navi.sh
+++ extras/setup_Navi.sh
@@ -3,5 +3,5 @@
 sudo bash -c "$(wget -O- https://raw.githubusercontent.com/denisidoro/navi/master/scripts/install)"
 
 if [ -d "$HOME/.local/share/navi/cheats/denisidoro__cheats/" ]; then
-    ln -fs $HOME/Dotfiles/Cheat_Sheet.cheat $HOME/.local/share/navi/cheats/denisidoro__cheats/Cheat_Sheet.cheat
+    ln -fs $HOME/Dotfiles/extras/Cheat_Sheet.cheat $HOME/.local/share/navi/cheats/denisidoro__cheats/Cheat_Sheet.cheat
 fi
