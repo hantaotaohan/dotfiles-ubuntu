d477bf942920f33955fbef9a71f9d67322c4aeb7
TaoTao <hantaotaohan@gmail.com>
2020-08-18 12:31:43 +0800

    Create setup_ImageMagick.sh

---
+++ extras/setup_ImageMagick.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt install imagemagick
+sudo sed -i 's/<policy domain="coder" rights="none" pattern="PDF" \/>/<policy domain="coder" rights="read|write" pattern="PDF" \/>/g' /etc/ImageMagick-6/policy.xml
