0139ab0cb5a5db10db4f64d941ace3eab4da76d7
TaoTao <hantaotaohan@gmail.com>
2020-08-28 13:45:57 +0800

    Create setup_arc_theme.sh

---
+++ extras/setup_arc_theme.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt install arc-theme
+
+if [ -f /etc/gtk-3.0/settings.ini ]; then
+sudo sed -i 's/gtk-theme-name = Ambiance/gtk-theme-name = Arc-Dark/g' /etc/gtk-3.0/settings.ini
+fi
