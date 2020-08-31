f973e5a76657e280f16d70b287fbabd21e4c7da2
TaoTao <hantaotaohan@gmail.com>
2020-08-31 13:30:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -673,11 +673,11 @@ echo -e "                                                                       
 #----------------------------------------------------------------------------------------#
 
         if [ -f "/usr/bin/rslsync" ]; then
-	    sudo usermod -aG $USER rslsync
-	    sudo usermod -aG rslsync $USER
-	    sudo chmod g+rw $HOME
-	    systemctl --user enable resilio-sync
-	    systemctl --user start resilio-sync
+	    sudo usermod -aG $USER rslsync &&\
+	    sudo usermod -aG rslsync $USER &&\
+	    sudo chmod g+rw $HOME &&\
+	    systemctl --user enable resilio-sync &&\
+	    systemctl --user start resilio-sync &&\
 	    sudo service resilio-sync start    
         fi
         echo -e "              ${green}[√] Rsync Successful${reset}\n"
