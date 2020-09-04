d0f9349f7bf213d73c1fdf690cb0714c23e12bfe
TaoTao <hantaotaohan@gmail.com>
2020-09-04 14:16:44 +0800

    Create setup_arc_icons.sh

---
+++ extras/setup_arc_icons.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git clone https://hub.fastgit.org/horst3180/arc-icon-theme --depth 1 && cd arc-icon-theme
+sudo cp -r Arc /usr/share/icons/
+sudo chmod +x /usr/share/icons/Arc
+sudo sed -i 's/gtk-icon-theme-name = ubuntu-mono-dark/gtk-icon-theme-name = Arc/g' /etc/gtk-3.0/settings.ini
