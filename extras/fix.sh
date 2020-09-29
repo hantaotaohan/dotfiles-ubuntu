252fe7779e94292c5995163a1ac7eb675dfc2b85
hantaotaohan <hantaotaohan@gmail.com>
2020-09-29 11:40:47 +0800

    sync

---
+++ extras/fix.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+bash setup_update_dunst.sh &&\
+bash setup_update_vim.sh &&\
+bash setup_i3gaps.sh &&\
+bash setup_arc_theme.sh &&\
+bash setup_arc_icons.sh &&\
+bash config_gtk.sh &&\
+bash setup_imagemagick.sh &&\
+bash setup_jupyter.sh &&\
+bash setup_navi.sh &&\
+bash setup_peek.sh &&\
+bash config_fcitx.sh &&\
+bash config_hosts.sh &&\
+bash config_i3_terminal.sh &&\
+bash config_autorun_offlineimap.sh
