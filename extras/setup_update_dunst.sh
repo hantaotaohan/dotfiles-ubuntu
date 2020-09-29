326a25b0671f3da58ee4c0743941eefe516ddffc
TaoTao <hantaotaohan@gmail.com>
2020-09-29 08:51:38 +0800

    Update setup_update_dunst.sh

--- extras/setup_update_dunst.sh
+++ extras/setup_update_dunst.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo apt install libdbus-1-dev libx11-dev libxinerama-dev libxrandr-dev libxss-dev libglib2.0-dev libpango1.0-dev libgtk-3-dev libxdg-basedir-dev libnotify-dev
-git clone https://hub.fastgit.org/dunst-project/dunst.git
-cd dunst
+git clone https://hub.fastgit.org/dunst-project/dunst.git $HOME/dunst
+cd $HOME/dunst
 make
 sudo make install
 cd ..
