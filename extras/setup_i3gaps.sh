82488839e8d69f289bcac5c59c68ef9acb298c65
TaoTao <hantaotaohan@gmail.com>
2020-09-29 12:53:17 +0800

    Update setup_i3gaps.sh

--- extras/setup_i3gaps.sh
+++ extras/setup_i3gaps.sh
@@ -3,4 +3,5 @@
 sudo add-apt-repository ppa:kgilmer/speed-ricer
 sudo apt-get update
 sudo apt install  i3-gaps-wm
+sudo apt-get -y -qq --purge remove rxvt-unicode > /dev/null 2>&1 
 sudo add-apt-repository --remove ppa:kgilmer/speed-ricer
