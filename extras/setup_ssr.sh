820a70d4cb938714519b61b4eba6a787c7df26f1
TaoTao <hantaotaohan@gmail.com>
2020-10-14 15:05:48 +0800

    Update setup_ssr.sh

--- extras/setup_ssr.sh
+++ extras/setup_ssr.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget -P $HOME/Desktop https://download.fastgit.org/shadowsocksrr/electron-ssr/releases/download/0.3.0-alpha.6/electron-ssr-0.3.0-alpha.6.deb
-sudo dpkg -i electron-ssr-0.3.0-alpha.6.deb
+sudo dpkg -i $HOME/Desktop/electron-ssr-0.3.0-alpha.6.deb
 sudo rm -rf $HOME/Desktop/electron-ssr-0.3.0-alpha.6.deb
