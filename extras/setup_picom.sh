f5486308500b34f8bbaa0f7cb2ff79f62437b45b
TaoTao <hantaotaohan@gmail.com>
2020-10-23 14:26:06 +0800

    Update setup_picom.sh

--- extras/setup_picom.sh
+++ extras/setup_picom.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-sudo apt install libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-de
+#!/bin/bash
+
+sudo apt install libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev  libpcre2-dev  libevdev-dev uthash-dev libev-dev libx11-xcb-dev
 sudo apt install meson
-git clone https://github.com/yshui/picom.git $HOME/picom
-cd $HOME/picom
-git submodule update --init --recursive
-meson --buildtype=release . build
-sudo ninja -C build
-cd ..
-rm -rf $HOME/picom
+git clone https://hub.fastgit.org/yshui/picom.git $HOME/picom && \
+cd $HOME/picom && \
+git submodule update --init --recursive && \
+meson --buildtype=release . build && \
+sudo ninja -C build && \
+sudo ninja -C build install && \
+cd .. && \
+sudo rm -rf $HOME/picom
