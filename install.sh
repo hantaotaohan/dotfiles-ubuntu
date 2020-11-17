bc743a5eff5fdc88fb6a69fb8c90ccde77221bda
TaoTao <hantaotaohan@gmail.com>
2020-11-17 10:04:35 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -490,8 +490,8 @@ echo -e "                                                                       
 	for app in "${gemApps[@]}"
 	do
         echo -e "              [*] Installing: $app";
-		gem sources --remove https://rubygems.org/
-		gem sources -a https://gems.ruby-china.com/
+		gem sources --remove https://rubygems.org/ > /dev/null 2>&1
+		gem sources -a https://gems.ruby-china.com/ > /dev/null 2>&1
 		sudo gem install $app > /dev/null 2>&1
 		#sudo gem install --local $Dotfiles_repo/bin/$app > /dev/null 2>&1
 		installSuccess $? $app
