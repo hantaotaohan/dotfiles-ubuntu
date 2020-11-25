7dd7d93940f5a2c932e564bd4772d24a4a2314df
TaoTao <hantaotaohan@gmail.com>
2020-11-25 15:03:50 +0800

    Update setup_tools.sh

--- setup/setup_tools.sh
+++ setup/setup_tools.sh
@@ -174,7 +174,7 @@ Jupyter() {
 }
 
 Navi() {
-    sudo bash -c "$(wget -O- https://raw.githubusercontent.com/denisidoro/navi/master/scripts/install)"
+    sudo bash -c "$(wget -O- https://raw.fastgit.org/denisidoro/navi/master/scripts/install)"
     if [ -d "$HOME/.local/share/navi/cheats/denisidoro__cheats/" ]; then
         ln -fs $Dotfiles_repo/cheatsheets/cheatsheets.cheat $HOME/.local/share/navi/cheats/denisidoro__cheats/cheatsheets.cheat
     else
