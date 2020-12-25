6c1632830736e68448110c3f795de7b6e5109c7d
TaoTao <hantaotaohan@gmail.com>
2020-12-25 16:16:02 +0800

    Delete wiki2html.sh

--- wiki2html.sh
+++
@@ -1,6 +0,0 @@
-cd $HOME/blog
-env HUGO_baseURL="$HOME/blog/public/" \
-    hugo --themesDir $HOME/blog/themes/ -t zdoc \
-    --config $HOME/blog/config/_default/config.toml \
-    --contentDir $HOME/blog/content/en \
-    -d $HOME/blog/public --quiet > /dev/null
