c017f871dcb029305db82fee6cd0f3ad5c17d405
TaoTao <hantaotaohan@gmail.com>
2021-02-03 15:03:16 +0800

    Update notify.sh

--- .config/neomutt/notify.sh
+++ .config/neomutt/notify.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+# Windows User 
+# In Powershell run > Install-Module -Name BurntToast
+
 #run OfflineIMAP once, with quiet interface
 #offlineimap -o -q -u quiet
 
 # Windows User 
 # In Powershell run > Install-Module -Name BurntToast
 
-#count new mail for every maildir
-# maildirnew="$HOME/Maildir/*/*/new/"
 maildirnew="$HOME/mail/bteb/INBOX/new/"
 maildirnew1="$HOME/mail/live/Inbox/new/"
 new="$(find $maildirnew -type f | wc -l)"
@@ -16,14 +17,10 @@ new1="$(find $maildirnew1 -type f | wc -l)"
 # maildirold="$HOME/Maildir/*/*/cur/"
 # old="$(find $maildirold -type f | wc -l)"
 
-if [ $new -gt 0 ] || [ $new1 -gt 0 ];
+if [ $new -gt 0 ] || [ $new1 -gt 0 ]
 then
-    if hash dunstify 2>/dev/null; 
-    then
-        dunstify -r 7777 -a "neomutt" -u c critical "New mail! \n\nBTEB: $new   HOTMAIL: $new1"
-    elif grep -q microsoft /proc/version;
-    then
-        powershell.exe -Command New-BurntToastNotification -Text "NewMail", "BTEB：$new",  "HOTMAIL：$new1"
-    fi
-else echo""
+     # dunstify -r 7777 -a "neomutt" normal  "New mail! \n\nBTEB: $new   HOTMAIL: $new1"
+     dunstify -r 7777 -a "neomutt" -u c critical "New mail! \n\nBTEB: $new   HOTMAIL: $new1"
 fi
+
+# powershell.exe -Command New-BurntToastNotification -Text "NewMail", "BTEB：$new",  "HOTMAIL：$new1"
